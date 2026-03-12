5335670b98d45ea71a188c9074851a3821866906
Steve Rosam <steve+quixers@quix.io>
2026-03-12 15:02:10 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -78,8 +78,10 @@ def main():
     )
     sdf.print(lambda a: f'AFTER tumbling window:: {a["value"]}')
 
-    # sdf = sdf.apply(lambda result: {"count": result["value"], "ts_start": result.get("ts_start")})
-    # sdf.print()
+    sdf = sdf.apply(lambda result: {"count": result["value"], "ts_start": result.get("ts_start")})
+
+    sdf.print(lambda a: f'AFTER adding ts_start:: {a["value"]}')
+
     # sdf.to_topic(output_topic)
 
     # With our pipeline defined, now run the Application
