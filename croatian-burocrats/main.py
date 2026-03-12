bbf21e32adb2a483312b75c54eb548c9d835694c
Steve Rosam <steve+quixers@quix.io>
2026-03-12 14:22:12 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -75,7 +75,9 @@ def main():
 
         # Emit updates for each incoming message
         .final()
-    ).apply(lambda result: ("count": result["value"], "ts_start": result.get("ts_start")))
+    )
+    
+    sdf.apply(lambda result: ("count": result["value"], "ts_start": result.get("ts_start")))
 
     sdf.print()
 
