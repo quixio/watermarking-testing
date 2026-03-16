2adc5f50f3ba38640f8516a820ae85280f60411a
Tomas Neubauer <tomas+quixers@quix.io>
2026-03-16 13:43:22 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -30,7 +30,7 @@ def main():
     # All replicas share the same consumer group so Kafka distributes
     # partitions between them automatically.
     app = Application(
-        consumer_group="colour_counter_v1.2",
+        consumer_group="burocrats_watermarking_" + os.environ["consumer_group"],
         auto_create_topics=True,
         auto_offset_reset="earliest",
     )
