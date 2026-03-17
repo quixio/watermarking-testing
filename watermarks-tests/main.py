8d6e27939ac995ea2a169251796ebf5749f94c0f
Daniel Lastic <daniel@quix.io>
2026-03-17 10:56:22 +0100

    Update SQL query

--- watermarks-tests/main.py
+++ watermarks-tests/main.py
@@ -62,8 +62,7 @@ def _(mo):
       ON wm.run_id = nm.run_id
       AND wm.colour = nm.colour
       AND wm.start = nm.start
-    WHERE wm.run_id > 'run_2026-03-17 12:27:00'
-      AND wm.run_id < 'run_2026-03-17 12:30:00'
+    WHERE wm.run_id > 'run_2026-03-17 09:45'
     GROUP BY wm.colour, wm.run_id
     ORDER BY wm.run_id DESC, wm.colour
     LIMIT 100
