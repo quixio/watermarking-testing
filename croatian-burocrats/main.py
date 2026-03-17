9647a33a8c9e9d3684d880a05c2ea2f24d3ef397
Ludvík Bazjuk <ludvik+quixers@quix.io>
2026-03-17 14:00:32 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -33,7 +33,7 @@ def main():
         consumer_group="burocrats_watermarking_" + os.environ["consumer_group"],
         auto_create_topics=True,
         auto_offset_reset="earliest",
-        processing_guarantee="exactly-once",
+        #processing_guarantee="exactly-once",
         commit_every=1000,
         commit_interval=10
     )
