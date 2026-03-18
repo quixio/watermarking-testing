1788ee6e3dfcc3bc1127a1c7ca47eac4ed65e9d7
Ludvík Bazjuk <ludvik+quixers@quix.io>
2026-03-18 15:02:54 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -95,7 +95,7 @@ def main():
     sdf.to_topic(output_topic)
 
     # With our pipeline defined, now run the Application
-     print("[STARTUP] calling app.run()", flush=True)  
+    print("[STARTUP] calling app.run()", flush=True)  
     app.run()
 
 
