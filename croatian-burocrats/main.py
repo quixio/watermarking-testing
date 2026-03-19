ae051818e310045022a39a291c6211c6ac8790ce
Ludvík Bazjuk <ludvik+quixers@quix.io>
2026-03-19 15:46:24 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -29,7 +29,7 @@ def main():
         processing_guarantee="exactly-once",
         commit_every=1000,
         commit_interval=10,
-        watermarks_reset_on_start=True,
+        watermarks_reset_on_start=False,
         watermarks_idle_partition_timeout=2.0,
         watermarks_idle_advance_after_ms=50_000,
     )
