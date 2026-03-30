0f4f544ebde242453065c085ffc951444170381a
Ludvík Bazjuk <ludvik+quixers@quix.io>
2026-03-30 08:47:21 +0000

    Edited 'main.py'

--- croatian-burocrats/main.py
+++ croatian-burocrats/main.py
@@ -56,7 +56,7 @@ def main():
 
     sdf = (
         sdf
-        .tumbling_window(duration_ms=timedelta(seconds=10), grace_ms=timedelta(seconds=2))
+        .tumbling_window(duration_ms=timedelta(seconds=10), grace_ms=timedelta(seconds=1))
         .agg(count=Count(), run_id=First("run_id"))
         .final()
     )
