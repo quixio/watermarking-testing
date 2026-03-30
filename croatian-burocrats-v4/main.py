e64477c7f37879fd32507bc4e9694cc5fe33ba52
Ludvík Bazjuk <ludvik+quixers@quix.io>
2026-03-30 11:11:05 +0000

    Edited 'main.py'

--- croatian-burocrats-v4/main.py
+++ croatian-burocrats-v4/main.py
@@ -32,7 +32,7 @@ def main():
     app = Application(
         consumer_group="burocrats_watermarking_v4_" + os.environ["consumer_group"],
         auto_create_topics=True,
-        auto_offset_reset="earliest",
+        auto_offset_reset="latest",
         processing_guarantee="at-least-once"
     )
 
